7487b418398b78cb3dd4d3e6669b5d328d3aa2b3
macarval <macarval@student.42sp.org.br>
2024-03-20 23:23:22 -0300

    [CPP00]: ex02 in progress...!

--- Cpp00/ex02/include/Account.hpp
+++ Cpp00/ex02/include/Account.hpp
@@ -18,45 +18,41 @@
 
 class Account {
 
+	public:
 
-public:
+		typedef Account		t;
 
-	typedef Account		t;
+		static int	getNbAccounts(void);
+		static int	getTotalAmount(void);
+		static int	getNbDeposits(void);
+		static int	getNbWithdrawals(void);
+		static void	displayAccountsInfos(void);
 
-	static int	getNbAccounts( void );
-	static int	getTotalAmount( void );
-	static int	getNbDeposits( void );
-	static int	getNbWithdrawals( void );
-	static void	displayAccountsInfos( void );
+		Account(int initial_deposit);
+		~Account(void);
 
-	Account( int initial_deposit );
-	~Account( void );
-
-	void	makeDeposit( int deposit );
-	bool	makeWithdrawal( int withdrawal );
-	int		checkAmount( void ) const;
-	void	displayStatus( void ) const;
+		void	makeDeposit(int deposit);
+		bool	makeWithdrawal(int withdrawal);
+		int		checkAmount(void) const;
+		void	displayStatus(void) const;
 
 
-private:
+	private:
 
-	static int	_nbAccounts;
-	static int	_totalAmount;
-	static int	_totalNbDeposits;
-	static int	_totalNbWithdrawals;
+		static int	_nbAccounts;
+		static int	_totalAmount;
+		static int	_totalNbDeposits;
+		static int	_totalNbWithdrawals;
 
-	static void	_displayTimestamp( void );
+		static void	_displayTimestamp(void);
 
-	int				_accountIndex;
-	int				_amount;
-	int				_nbDeposits;
-	int				_nbWithdrawals;
+		int				_accountIndex;
+		int				_amount;
+		int				_nbDeposits;
+		int				_nbWithdrawals;
 
-	Account( void );
-
+		Account(void);
 };
-
-
 
 // ************************************************************************** //
 // vim: set ts=4 sw=4 tw=80 noexpandtab:                                      //
@@ -64,6 +60,5 @@ private:
 // -*- mode: c++-mode;                                                       -*-
 // -*- fill-column: 75; comment-column: 75;                                  -*-
 // ************************************************************************** //
-
 
 #endif /* __ACCOUNT_H__ */
--- Cpp00/ex02/src/Account.cpp
+++ Cpp00/ex02/src/Account.cpp
@@ -6,7 +6,73 @@
 /*   By: macarval <macarval@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 22:44:22 by macarval          #+#    #+#             */
-/*   Updated: 2024/03/20 22:44:23 by macarval         ###   ########.fr       */
+/*   Updated: 2024/03/20 23:22:29 by macarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account (int initial_deposit)
+{
+	this->_amount = initial_deposit;
+	this->_accountIndex = 0;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+
+	_totalAmount += initial_deposit;
+	_totalNbDeposits++;
+}
+
+Account::~Account(void)
+{
+	_nbAccounts--;
+}
+
+int	Account::getNbAccounts(void)
+{
+	return (_nbAccounts);
+}
+int	Account::getTotalAmount(void)
+{
+	return (_totalAmount);
+}
+int	Account::getNbDeposits(void)
+{
+	return (_totalNbDeposits);
+}
+int	Account::getNbWithdrawals(void)
+{
+	return (_totalNbWithdrawals);
+}
+void	Account::displayAccountsInfos(void)
+{
+
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	this->_amount += deposit;
+}
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	this->_nbWithdrawals += withdrawal;
+	return (true);
+}
+int		Account::checkAmount(void) const
+{
+	return (0);
+}
+void	Account::displayStatus(void) const
+{
+
+}
+
+void	Account::_displayTimestamp(void)
+{
+
+}
