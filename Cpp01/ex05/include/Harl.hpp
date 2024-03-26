10b6eb8dbba474dfd3324e14ca87805bc7e71885
macarval <macarval@student.42sp.org.br>
2024-03-26 17:38:48 -0300

    [CPP01]: ex05 Init!

---
+++ Cpp01/ex05/include/Harl.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macarval <macarval@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/26 17:34:22 by macarval          #+#    #+#             */
+/*   Updated: 2024/03/26 17:36:52 by macarval         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+# include <fstream>
+# include <sstream>
+
+// Color codes as global variables
+const std::string RESET = "\033[0m";
+const std::string RED = "\033[31;1m";
+const std::string GREEN = "\033[32;1m";
+const std::string YELLOW = "\033[33;1m";
+const std::string BLUE = "\033[34;1m";
+const std::string PURPLE = "\033[35;1m";
+const std::string CYAN = "\033[36;1m";
+const std::string GRAY = "\033[37;1m";
+
+class Harl
+{
+	private:
+
+	public:
+
+
+};
+
+#endif
