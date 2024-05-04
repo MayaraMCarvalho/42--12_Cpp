a3593a21b32d5db989ce8789abdba89d84607ddb
macarval <macarval@student.42sp.org.br>
2024-05-04 15:27:17 -0300

    [CPP07]: ex01 Done!

--- Cpp07/ex01/include/iter.hpp
+++ Cpp07/ex01/include/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: macarval <macarval@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 11:16:10 by macarval          #+#    #+#             */
-/*   Updated: 2024/05/04 15:16:41 by macarval         ###   ########.fr       */
+/*   Updated: 2024/05/04 15:26:22 by macarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,22 +26,24 @@ const std::string PURPLE = "\033[35;1m";
 const std::string CYAN = "\033[36;1m";
 const std::string GRAY = "\033[37;1m";
 
-template <typename T, typename F>
-void iter(T *arr, size_t size, F func)
+template <typename T>
+void iter(T *arr, size_t size, void (*func)(T const &))
 {
 	for (size_t i = 0; i < size; ++i)
 		func(arr[i]);
 }
 
 template <typename T>
-void up(T &a) {
-	std::string color[] = { "\033[0;31m",
-							"\033[0;32m",
-							"\033[0;33m",
-							"\033[0;34m",
-							"\033[0;35m" };
+void up(T const &a) {
+	std::string color[] = { RED,
+							GREEN,
+							YELLOW,
+							BLUE,
+							PURPLE,
+							CYAN,
+							GRAY };
 
-	std::cout << "\t\t\t" << color[std::rand() % 5] << a << "\033[0m " << std::endl;
+	std::cout << "\t\t\t" << color[rand() % 7] << a << RESET << std::endl;
 }
 
 #endif
